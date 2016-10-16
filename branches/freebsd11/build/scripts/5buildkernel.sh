50f34b3e74d206383a9abb56f5a9ac57a17cfd7f
t1n1wall <t1n1wall@1923a51e-bcc9-4791-ba76-884aabe27594>
2016-10-16 01:47:17 +0000

    remove hardware 'asr' from kernel , remove HYPERV from kernel , both don't exist in Freebsd11 comment out two non-functional kernel patches for now

--- branches/freebsd11/build/scripts/5buildkernel.sh
+++ branches/freebsd11/build/scripts/5buildkernel.sh
@@ -16,7 +16,7 @@ fi
 # patch kernel / sources
 		cd $MW_BUILDPATH/tmp
 # revert checksum changes submitted as kern/203585
-		patch < $MW_BUILDPATH/freebsd11/build/patches/kernel/ip6_checksums.patch
+		#patch < $MW_BUILDPATH/freebsd11/build/patches/kernel/ip6_checksums.patch
 # apuled kern/189772
 		mkdir apuled
 		cd apuled
@@ -51,7 +51,7 @@ fi
 # fix for noika ip120 intel nic
 		patch < $MW_BUILDPATH/freebsd11/build/patches/kernel/if_em.c.patch
 #
-		patch < $MW_BUILDPATH/freebsd11/build/patches/kernel/if_fxp.c.patch
+		#patch < $MW_BUILDPATH/freebsd11/build/patches/kernel/if_fxp.c.patch
 # glxsb crypto speed increase kern/132622
 		patch < $MW_BUILDPATH/freebsd11/build/patches/kernel/glxsb.c.orig.patch
 # ipsec patch for l2tp to work
